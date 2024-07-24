bb7d37c9a9f2455bf6507fd8838b8dbd069f860a
JordiLara <165904939+JordiLara@users.noreply.github.com>
2024-07-24 12:02:23 +0200

    add solution to 1.2 exercise

--- Exercise 1.2/index.js
+++ Exercise 1.2/index.js
@@ -8,3 +8,13 @@ function potConduir(edat) {
 
 console.log(potConduir(43));
 
+/* 1.2 - Ús amb operadors de comparació: Escriu una expressió que utilitzi l'operador ternari per determinar quin dels dos nombres donats (num1 i num2) és més gran. Si num1 és més gran, retorna 'num1 és més gran'. Si no, retorna 'num2 és més gran'.*/
+
+let num1 = 18;
+let num2 = 22;
+
+const comparar = num1 > num2 ? 'num1 és més gran' : 'num2 és més gran';
+
+console.log(comparar);
+
+
