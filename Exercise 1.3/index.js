8cddfdc4003eb0c661da6bbf14afd968429e7327
JordiLara <165904939+JordiLara@users.noreply.github.com>
2024-07-25 13:53:23 +0200

    add small visual changes

--- Exercise 1.3/index.js
+++ Exercise 1.3/index.js
@@ -32,9 +32,11 @@ function esperarISaludar(nom, callback) {
   
 // Exemple:
  
-esperarISaludar('Pepito', function(nomSaludo){
+esperarISaludar('Don José', function(nomSaludo){
 console.log('Hola ' + nomSaludo)
 });
+
+// 2 Estrellas
 
 /* 1.4 - Callbacks amb arrays: Escriu una funció processarElements que accepti dos paràmetres: un array i una funció de callback. La funció processarElements ha d'invocar la funció de callback per cada element de l'array. */
 
