5131b88cc358b5a8b2050b74ab583714380271fc
JordiLara <165904939+JordiLara@users.noreply.github.com>
2024-07-29 14:18:08 +0200

    add exercise 1.2 and solution

--- Exercise 1.6/index.js
+++ Exercise 1.6/index.js
@@ -8,3 +8,11 @@ noms.forEach(myFunction);
 function myFunction(item, index) {
   console.log ( index + ": " + item); 
 }
+
+/* 1.2 - for-of: Teniu una array de noms. Utilitza un bucle for-of per a imprimir cada nom a la consola: let noms = ['Anna', 'Bernat', 'Clara']; */
+
+let noms1 = ['Anna', 'Bernat', 'Clara'];
+
+for (let x of noms1) {
+    console.log(x)
+}
