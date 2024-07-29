67e1ccf73e5e8c5a8475846390add1c6767b5c9c
JordiLara <165904939+JordiLara@users.noreply.github.com>
2024-07-29 17:03:42 +0200

    add some minor corrections and language changes

--- Exercise 1.7/index.js
+++ Exercise 1.7/index.js
@@ -8,7 +8,7 @@ const delayMessage = () => {
             resolve('Hola, món');
         }, 2000);
     });
-};
+}
     
 delayMessage()
 .then((message) => console.log(message));
@@ -22,17 +22,17 @@ const delayMessage1 = () => {
             resolve('Hola, món');
         }, 2000);
     });
-};
+}
     
 delayMessage1()
 .then((message) => console.log(message));
 
 /* 1.3 - Promesa amb reject: Crea una promesa que es resolgui després de 2 segons si l'input és igual a 'Hola', i que la rebutgi si l'input és qualsevol altra cosa. */
 
-const delayMessage2 = (mensaje) => {
+const delayMessage2 = (message) => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            if (mensaje == 'Hola') {
+            if (message == 'Hola') {
                 resolve('La promesa es aceptada. Hola!');
             } else {
                 reject('La promesa ha sido rechazada.');
@@ -42,5 +42,5 @@ const delayMessage2 = (mensaje) => {
 }
 
 delayMessage2('Hello') // modificar aquí para que la promesa sea aceptada o rechazada.
-    .then((mensaje) => console.log(mensaje))
+    .then((message) => console.log(message))
     .catch((error) => console.error(error));
